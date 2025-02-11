b036837c3b20cf9c6ec2c142a7e3aecc7a55c4d7
Vijay K. <vijay@mevijay.com>
2025-02-11 09:51:26 +0530

    Update check.sql

--- docs/sql/check.sql
+++ docs/sql/check.sql
@@ -1,10 +1,5 @@
-UPDATE employees
-SET salary = salary * 1.1
-WHERE TENANT_ID = 1234;
+UPDATE employees SET salary = salary * 1.1 WHERE TENANT_ID = 1234;
 
-DELETE FROM employees
-WHERE TENANT_ID = 1234;
+DELETE FROM employees WHERE TENANT_ID = 1234; 
 
-UPDATE employees
-SET NAME = "vijay";
-WHERE TENANT_ID = 1234;
+UPDATE employees SET NAME = "vijay" WHERE TENANT_ID = 1234;
