5d774bc773a0a58d654b45b30ecc4db5564e29a6
Vijay K. <vijay@mevijay.com>
2025-02-11 15:50:03 +0530

    Update check.sql

--- docs/sql/check.sql
+++ docs/sql/check.sql
@@ -1,5 +1,27 @@
-UPDATE employees SET salary = salary * 1.1 WHERE TENANT_ID = 1234;
+--liquibase formatted sql
 
-DELETE FROM employees WHERE TENANT_ID = 1234; 
+--changeset vijay:20240319-001
+--comment: Update employee salaries with 10% increase
+update employees
+set salary = salary * 1.1
+where tenant_id = 1234;
+--rollback update employees set salary = salary / 1.1 where tenant_id = 1234;
 
-UPDATE employees SET NAME = "vijay" WHERE TENANT_ID = 1234;
+--changeset vijay:20240319-002
+--comment: Delete employees for specific tenant
+delete from employees
+where tenant_id = 1234;
+--rollback INSERT statements would be needed for deleted data
+
+--changeset vijay:20240319-003
+--comment: Update employee name
+UPDATE employees
+set name = 'vijay'
+where tenant_id = 1234;
+--rollback update employees set name = 'previous_name' where tenant_id = 1234;
+
+--changeset vijay:20240319-004
+--comment: Insert new employee
+insert into employees (id, name, salary)
+values (1, 'John Doe', 50000);
+--rollback delete from employees where id = 1;
